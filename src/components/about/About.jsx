b88add91ba1018c3a5546993d3f145746411de55
VincentBGojo <vincent.bouchard92@gmail.com>
2022-05-13 04:36:30 +0200

    About component, About CSS and responsive

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,10 +1,49 @@
 import React from 'react'
 import "./about.css"
+import ME from '../../assets/hunter.png'
+import {FaAward} from 'react-icons/fa'
+import {DiJavascript1} from 'react-icons/di'
+import {MdOutlineNetworkCheck} from 'react-icons/md'
+import {FaInfinity} from 'react-icons/fa'
 
 const About = () => {
   return (
     <section id='about'>
-      A propos
+      <h5>Ce qu'il faut savoir</h5>
+      <h2>Sur moi</h2>
+
+      <div className="container about__container">
+        <div className="about__me">
+          <div className="about__me-image">
+            <img src={ME} alt="" />
+          </div>
+
+        </div>
+        <div className="about__content">
+          <div className="about__cards">
+            <article className='about__card'>
+              <FaAward className='about__icon'/>
+              <h5>Expérience</h5>
+              <small>1 an et +</small>
+            </article>
+            <article className='about__card'>
+              <DiJavascript1 className='about__icon'/>
+              <h5>Projets</h5>
+              <small>6</small>
+            </article>
+            <article className='about__card'>
+              <MdOutlineNetworkCheck className='about__icon'/>
+              <h5>Motivation</h5>
+              <small><FaInfinity /></small>
+            </article>
+          </div>
+          <p>
+            Passionné du développement web, j'aime créer tout type de projet et affronter les nombreux obstacles que ces derniers génèrent. Le travail de groupe et l'apprentissage sont mes deux principaux moteurs. Je ne demande qu'à enrichir mes connaissances du code et travailler sur des projets complexes et variés.
+          </p>
+
+          <a href="#contact" className='btn btn-primary'>Me contacter</a>
+        </div>
+      </div>
     </section>
   )
 }
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import CTA from './CTA'
 import './header.css'
-import ME from '../../assets/me.png'
+import ME from '../../assets/profil.png'
 import HeaderSocials from './HeaderSocials'
 
 
@@ -12,7 +12,7 @@ const header = () => {
           <h5>Salut je suis</h5>
           <h1>Vincent Bouchard</h1>
           <h5 className="text-light">
-            Développeur Front-end
+            Développeur Front-end spécialisé React
           </h5>
           <CTA />
            <HeaderSocials />
