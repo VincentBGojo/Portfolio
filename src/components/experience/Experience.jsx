0936565bf88edc99f6bcef48518a1f02930b3da1
VincentBGojo <vincent.bouchard92@gmail.com>
2022-05-14 17:12:25 +0200

    Experience component, experience css and responsive

--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -1,10 +1,91 @@
 import React from 'react'
 import './experience.css'
+import {BsPatchCheckFill} from 'react-icons/bs'
 
 const Experience = () => {
   return (
     <section id='experience'>
-      Experience
+      <h5>Mon expérience</h5>
+      <h2>Compétences</h2>
+
+      <div className="container experience__container">
+
+      {/* Frontend section */}
+        <div className="experience__frontend">
+          <h3>Développement Frontend</h3>
+          <div className='experience__content'>
+            <article className='experience__details'>
+              <BsPatchCheckFill className="experience__details-icon" />
+              <div>
+              <h4>HTML/CSS</h4>
+              </div>
+            </article>   
+            <article className='experience__details'>
+              <BsPatchCheckFill className="experience__details-icon" />
+            <div>
+              <h4>Javascript</h4>
+            </div>
+            </article>
+            <article className='experience__details'>
+              <BsPatchCheckFill className="experience__details-icon" />
+              <div>
+              <h4>React</h4>
+              </div>
+            </article>
+            <article className='experience__details'>
+              <BsPatchCheckFill className="experience__details-icon" />
+              <div>
+              <h4>Boostrap/Bulma</h4>
+              </div>
+            </article>
+            <article className='experience__details'>
+              <BsPatchCheckFill className="experience__details-icon" />
+              <div>
+              <h4>Redux</h4>
+              </div>
+            </article>
+
+          </div>
+        </div>
+        {/* Backend section */}
+        <div className="experience__backend">
+          <h3>Développement Backend</h3>
+          <div className='experience__content'>
+            <article className='experience__details'>
+              <BsPatchCheckFill className="experience__details-icon" />
+              <div>
+              <h4>PHP</h4>
+              </div>
+            </article>
+            <article className='experience__details'>
+              <BsPatchCheckFill className="experience__details-icon" />
+              <div>
+              <h4>MySql</h4>
+              </div>
+            </article>
+            <article className='experience__details'>
+              <BsPatchCheckFill className="experience__details-icon" />
+              <div>
+              <h4>Api rest</h4>
+              </div>
+            </article>
+            <article className='experience__details'>
+              <BsPatchCheckFill className="experience__details-icon"/>
+              <div>
+              <h4>MongoDB</h4>
+              </div>
+            </article>
+            <article className='experience__details'>
+              <BsPatchCheckFill className="experience__details-icon" />
+              <div>
+              <h4>Laravel</h4>
+              </div>
+            </article>
+
+          </div>
+
+        </div>
+      </div>
     </section>
   )
 }
